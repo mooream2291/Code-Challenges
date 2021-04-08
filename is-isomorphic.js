eb7e8a15dc13d76f8bc88083f772d8047339062b
Ashley Moore <mooream2291@gmail.com>
2021-04-08 00:09:21 -0400

    is isomorphic

---
+++ is-isomorphic.js
@@ -0,0 +1,26 @@
+// 'use strict'
+
+// algorithm
+
+// declare a function that takes in two strings
+
+//  split each character in the strings
+
+//  comparisons to make:
+
+//  does a character exist more than once in string 1
+
+//  does a character exist more than once in string 2
+
+//  do the dupes exist at the same indices in each string?
+
+//  iterate through string 1 and store it in a hash map
+
+//  iterate through string 2 and compare the current index to the key in the map, IF the key already exists, nothing will happen, IF it doesn't the value will be replaced.
+
+// CODE:
+
+// function isIsomorphic  (s, t) {
+
+//  s.split("")
+// }
