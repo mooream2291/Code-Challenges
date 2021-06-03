0480d52f61b92d1590f1d345eb91ab31d24bc44e
Ashley Moore <mooream2291@gmail.com>
2021-06-02 21:06:27 -0400

    2 queues as a stack solution

--- stack-using-queue.js
+++ stack-using-queue.js
@@ -1,30 +1,45 @@
-//Implement a last in first out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal queue (push, top, pop, and empty).
+//Implement a last in first out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal queue (push, peek/top, pop, and empty/return true or false).
+
+//shift/pop removes from queue
+//unshift/push adds them
 
 //create stack data structure
 
+//1. When setting a variable to a property on an obhect, it is being set to the reference in memory NOT the actual object
+//2. Queue adds from the right and remove from the left
+//3.  Stack adds from the left and removes from the right
+
 class Stack {
   constructor() {
-    this.items = [];
+    this.current = [];
+    this.reserve = [];
     this.count = 0;
   }
 
-
   //add element to the top of the stack
-    push(element) {
-    this.queue[this.count] = element
-    this.count += 1;
-    return this.count -1
+  //something to keep in mind: what kind of errors can happen here ALWAYS consider there being null values and if this error should be handled
+  push(element) {
+    this.current.push(element)
   }
 
-  //return and remove top element in stack
-  //return undefined if stack is empty
   pop() {
-    if(this.count == 0) return undefined
-    let deleteItem = this.items[this.count -1]
-    this.count -= 1
-    return deleteItem
+    for(let i = 0; i < this.current.length -1; i++) {
+      this.reserve.push(this.current.pop());
+    }
+    let hold = this.reserve;
+    this.reserve = this.current;
+    this.current = hold;
+    return this.reserve.pop()
   }
+
+  empty() {
+    return count == 0;
   }
+
+  peek() {
+    return 
+  }
+}
 
 const stack = new Stack()
 
