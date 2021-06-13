935762360cb0fda8218c2799200f7aff3a6b5af5
Ashley Moore <mooream2291@gmail.com>
2021-06-13 12:26:32 -0400

    binary-tree-paths

---
+++ add-nums.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+//try to do without a loop or recursion which would be O(1) runtime
+function addNums(num) {
+
+  //separate digits into individual strings (is this necessary?)
+  let newNums = num.toString().split('');
+  
+ 
+  
+  return 
+}
---
+++ binary-tree-paths.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//Given the root of a binary tree, return all root-to-leaf paths in any order.
+
+var binaryTreePaths = function(root) {
+    let result = [];
+      function getPaths(root, weight) {
+        if (root.left == null && root.right == null) {
+          weight += root.val;
+          result.push(weight);
+          return;
+        }
+        getPaths(root.left, weight + root.val);
+        getPaths(root.left, weight + root.val);
+      }
+      return result;
+  };
