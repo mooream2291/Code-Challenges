2d9f046c7798d0fbdb87f5724555c1ab16f282f2
Ashley Moore <mooream2291@gmail.com>
2021-06-28 13:02:30 -0400

    happy number

--- happy-number.js
+++ happy-number.js
@@ -1,12 +1,21 @@
 'use strict';
 
 const isHappy = (n) => {
+//figure out returning true or false
+//returning true IF sum of sqaures = 1
+//returning false if sum of sqaures will never = 1
 
-  let n = n.split('')
+//to return false:
+  //chow do I check to see if the result of the function will be stuck in a never ending while loop?
 
-  let result = 0
-  
-  while (n !==1) {
-   for (let i = 0; i < n.length; i++) {
-   result += Math.pow(n[i], 2);
-}
+  //recursive function
+  const get_sum_of_squares = (n, sum) => {
+    //if statement is base case, this problem is an example of tail end recursion
+     if (n == 0) return sum
+     let firstNum = n % 10;
+     let restOfNum = n / 10;
+     return get_sum_of_squares(restOfNum, firstNum.pow(2) + sum)
+    }
+    if (sum === 1) return true;
+    if (sum >1 && sum <= 4) return false;
+  }
